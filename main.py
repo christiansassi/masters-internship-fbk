16df19359bfea61b461d8bf3a973d9b1a2408a4f
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2025-07-17 12:01:07 +0200

    combined training + deployment (partial)

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import config
 import utils
 
 from models import load_wide_deep_networks, load_threshold_networks
+
 from federated_server import Server
 from federated_client import generate_iid_clients
 
@@ -14,15 +15,18 @@ if __name__ == "__main__":
     
     utils.clear_console() 
 
-    # Generate clients
-    clients = generate_iid_clients()
-
     # Load wide deep networks
     wide_deep_networks = load_wide_deep_networks()
 
     # Load threshold networks
     threshold_networks = load_threshold_networks()
     
+    # Generate clients
+    clients = generate_iid_clients(
+        wide_deep_networks=wide_deep_networks,
+        threshold_networks=threshold_networks
+    )
+
     # Create server
     server = Server(
         clients=clients,
@@ -37,3 +41,7 @@ if __name__ == "__main__":
     if not len(threshold_networks):
         # Federated learning (threshold networks)
         server.federated_learning(label=THRESHOLD_NETWORKS_LABEL)
+    
+    # Simulate deploy
+    for client in clients:
+        client.test_network()
