new normalization + fixed mask + new wide deep network train/val/eval

federated_client.py
@@ -23,6 +23,8 @@ import uuid
 
 import matplotlib.pyplot as plt
 
+import pickle
+
 class Client:
     def __init__(
         self,
@@ -129,7 +131,7 @@ class Client:
         self.score = float("-inf")
 
         self.input_mask = [list(GLOBAL_INPUTS).index(x) for x in self.inputs]
-        self.output_mask = [[list(self.inputs).index(output) for output in outputs] for outputs in active_stages]
+        self.output_mask = [[list(self.inputs).index(output) for output in outputs] for outputs in active_stages if len(outputs)]
 
         self.val_mask = torch.zeros(BATCH_SIZE, WINDOW_PAST, len(GLOBAL_INPUTS))
         self.val_mask[:, :, self.input_mask] = 1
@@ -150,12 +152,12 @@ class Client:
 
     def train_model_f_extractor_and_sensor(self, model_f_extractor: ModelFExtractor, verbose: bool = False) -> tuple:
 
-        self.model_f_extractor.load_state_dict(model_f_extractor.state_dict())
-        
         self.model_f_extractor.to(DEVICE)
 
         for model_sensor in self.model_sensors:
             model_sensor.to(DEVICE)
+
+        self.model_f_extractor.load_state_dict(deepcopy(model_f_extractor.state_dict()))
 
         min_train_loss = float("inf")
         min_val_loss = float("inf")
@@ -206,7 +208,7 @@ class Client:
                 w_in = torch.from_numpy(w_in).float().to(DEVICE)
 
                 # Reset gradients
-                self.optimizer.zero_grad()
+                self.model_f_extractor.zero_grad()
 
                 # Forward pass through the feature extractor
                 x = self.model_f_extractor(w_in, train_mask)
@@ -221,6 +223,8 @@ class Client:
 
                     w_out = df_out.reshape(batch_size, WINDOW_PRESENT, -1)
                     w_out = torch.from_numpy(w_out).float().to(DEVICE)
+
+                    model_sensor.zero_grad()
 
                     y = model_sensor(x)
 
@@ -293,16 +297,16 @@ class Client:
             if val_loss < min_val_loss:
                 min_val_loss = val_loss
 
-                best_model_f_extractor = self.model_f_extractor.state_dict()
-                best_model_sensors = [model_sensor.state_dict() for model_sensor in self.model_sensors]
+                best_model_f_extractor = deepcopy(self.model_f_extractor.state_dict())
+                best_model_sensors = [deepcopy(model_sensor.state_dict()) for model_sensor in self.model_sensors]
 
             # Decay Learning Rate, pass validation loss for tracking at every epoch
             self.scheduler.step(val_loss)
 
-        self.model_f_extractor.load_state_dict(best_model_f_extractor)
+        self.model_f_extractor.load_state_dict(deepcopy(best_model_f_extractor))
         
         for model_sensor, best_model_sensor in zip(self.model_sensors, best_model_sensors):
-            model_sensor.load_state_dict(best_model_sensor)
+            model_sensor.load_state_dict(deepcopy(best_model_sensor))
 
         self.log(" " * 100, end="\r", verbose=verbose)
         self.log(f"Training loss: {min_train_loss} | Validation loss: {min_val_loss}", verbose=verbose)
@@ -311,12 +315,12 @@ class Client:
 
     def eval_model_f_extractor_and_sensor(self, model_f_extractor: ModelFExtractor, verbose: bool = False) -> float:
         
-        self.model_f_extractor.load_state_dict(model_f_extractor.state_dict())
-
         self.model_f_extractor.to(DEVICE)
 
         for model_sensor in self.model_sensors:
             model_sensor.to(DEVICE)
+
+        self.model_f_extractor.load_state_dict(deepcopy(model_f_extractor.state_dict()))
 
         # Evaluation
         self.model_f_extractor.eval()
@@ -375,6 +379,7 @@ class Client:
 
         return -eval_loss
 
+    #TODO
     def train_pred_error_model(self, verbose: bool = False):
         
         self.model_f_extractor.to(DEVICE)
@@ -483,12 +488,12 @@ class Client:
                 w_in = torch.from_numpy(df_in[:, :, None]).float().to(DEVICE)
                 w_out = torch.from_numpy(df_out[:, :, None]).float().to(DEVICE)
 
-                optimizer.zero_grad()
+                self.pred_error_model.zero_grad()
 
                 y = self.pred_error_model(w_in.float().to(DEVICE)).abs()
 
                 if torch.all(y == 0).item():
-                    optimizer.zero_grad()
+                    self.pred_error_model.zero_grad()
 
                     self.pred_error_model.apply(lambda model: model.reset_parameters() if isinstance(model, nn.Conv1d) or isinstance(model, nn.Linear) else None)
 
@@ -512,80 +517,87 @@ class Client:
             if train_loss < min_train_loss:
                 min_train_loss = train_loss
 
-                best_pred_error_model = self.pred_error_model.state_dict()
+                best_pred_error_model = deepcopy(self.pred_error_model.state_dict())
         
-        self.pred_error_model.load_state_dict(best_pred_error_model)
+        self.pred_error_model.load_state_dict(deepcopy(best_pred_error_model))
 
         self.log(" " * 100, end="\r", verbose=verbose)
         self.log(f"Training loss: {min_train_loss}", verbose=verbose)
 
         return -min_train_loss
 
+    #TODO
     def calculate_threshold_base(self):
 
         criterion = nn.MSELoss(reduction="none")
 
-        losses = []
+        losses = [[] for _ in range(len(self.model_sensors))]
 
         with torch.no_grad():
 
             steps = len(self.val_input_indices) // BATCH_SIZE
 
             for step in range(0, steps):
-
-                df_in = self.df_val[self.val_input_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()]
-                df_out = self.df_val[self.val_output_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()][:, self.output_mask]
-
+                
                 # Input
+                df_in = self.df_val[self.val_input_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()]
+
                 w_in = np.zeros((len(df_in), len(GLOBAL_INPUTS)), dtype=np.float32)
                 w_in[:, self.input_mask] = df_in
 
                 w_in = w_in.reshape(BATCH_SIZE, WINDOW_PAST, -1)
                 w_in = torch.from_numpy(w_in).float().to(DEVICE)
-
-                # Output
-                w_out = df_out.reshape(BATCH_SIZE, WINDOW_PRESENT, -1)
-                w_out = torch.from_numpy(w_out).float().to(DEVICE)
 
                 # Forward pass through the feature extractor
                 x = self.model_f_extractor(w_in, self.val_mask)
 
                 # Forward pass through the sensor head
-                y = self.model_sensor(x)
+                for index, (model_sensor, output_mask) in enumerate(zip(self.model_sensors, self.output_mask)):
+                    
+                    # Output
+                    df_out = self.df_val[self.val_output_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()][:, output_mask]
 
-                # Compute loss
-                loss = torch.mean(criterion(y, w_out), dim=2)
+                    w_out = df_out.reshape(BATCH_SIZE, WINDOW_PRESENT, -1)
+                    w_out = torch.from_numpy(w_out).float().to(DEVICE)
 
-                losses.extend(scipy.signal.medfilt(loss.detach().cpu().numpy()[:, 0].flatten(), kernel_size=MED_FILTER_LAG))
+                    y = model_sensor(x)
+
+                    # Compute loss
+                    loss = torch.mean(self.criterion(y, w_out), dim=2)
+                
+                    losses[index].append(scipy.signal.medfilt(loss.detach().cpu().numpy()[:, 0].flatten(), kernel_size=MED_FILTER_LAG))
             
             steps = len(self.test_input_indices) // BATCH_SIZE
 
             for step in range(0, steps):
-
-                df_in = self.df_test[self.test_input_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()]
-                df_out = self.df_test[self.test_output_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()][:, self.output_mask]
-
+                
                 # Input
+                df_in = self.df_test[self.test_input_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()]
+
                 w_in = np.zeros((len(df_in), len(GLOBAL_INPUTS)), dtype=np.float32)
                 w_in[:, self.input_mask] = df_in
 
                 w_in = w_in.reshape(BATCH_SIZE, WINDOW_PAST, -1)
                 w_in = torch.from_numpy(w_in).float().to(DEVICE)
 
-                # Output
-                w_out = df_out.reshape(BATCH_SIZE, WINDOW_PRESENT, -1)
-                w_out = torch.from_numpy(w_out).float().to(DEVICE)
-
                 # Forward pass through the feature extractor
                 x = self.model_f_extractor(w_in, self.eval_mask)
 
                 # Forward pass through the sensor head
-                y = self.model_sensor(x)
+                for index, (model_sensor, output_mask) in enumerate(zip(self.model_sensors, self.output_mask)):
+                    
+                    # Output
+                    df_out = self.df_test[self.test_output_indices[step * BATCH_SIZE: step * BATCH_SIZE + BATCH_SIZE].flatten()][:, output_mask]
+                    
+                    w_out = df_out.reshape(BATCH_SIZE, WINDOW_PRESENT, -1)
+                    w_out = torch.from_numpy(w_out).float().to(DEVICE)
 
-                # Compute loss
-                loss = torch.mean(criterion(y, w_out), dim=2)
+                    y = model_sensor(x)
 
-                losses.extend(scipy.signal.medfilt(loss.detach().cpu().numpy()[:, 0].flatten(), kernel_size=MED_FILTER_LAG))
+                    # Compute loss
+                    loss = torch.mean(criterion(y, w_out), dim=2)
+
+                    losses[index].append(scipy.signal.medfilt(loss.detach().cpu().numpy()[:, 0].flatten(), kernel_size=MED_FILTER_LAG))
 
         err_mean = np.mean(losses)
         err_std = np.std(losses)
@@ -896,24 +908,14 @@ class Client:
         plt.show()
 
     def set_model_f_extractor(self, model_f_extractor: ModelFExtractor | OrderedDict):
-        self.model_f_extractor.load_state_dict(model_f_extractor.state_dict() if isinstance(model_f_extractor, ModelFExtractor) else model_f_extractor)
+        self.model_f_extractor.load_state_dict(deepcopy(model_f_extractor.state_dict()) if isinstance(model_f_extractor, ModelFExtractor) else model_f_extractor)
 
-    def get_model_f_extractor(self) -> ModelFExtractor:
-        return deepcopy(self.model_f_extractor)
-    
     def set_model_sensors(self, model_sensors: list[ModelSensors | OrderedDict]):
-
         for model_sensor, loaded_model_sensor in zip(self.model_sensors, model_sensors):
-            model_sensor.load_state_dict(loaded_model_sensor.state_dict() if isinstance(loaded_model_sensor, ModelSensors) else loaded_model_sensor)
-
-    def get_model_sensor(self) -> list[ModelSensors]:
-        return deepcopy(self.model_sensors)
+            model_sensor.load_state_dict(deepcopy(loaded_model_sensor.state_dict()) if isinstance(loaded_model_sensor, ModelSensors) else loaded_model_sensor)
 
     def set_pred_error_model(self, pred_error_model: PredErrorModel | OrderedDict):
-        self.pred_error_model.load_state_dict(pred_error_model.state_dict() if isinstance(pred_error_model, PredErrorModel) else pred_error_model)
-
-    def get_pred_error_model(self) -> PredErrorModel:
-        return deepcopy(self.pred_error_model)
+        self.pred_error_model.load_state_dict(deepcopy(pred_error_model.state_dict()) if isinstance(pred_error_model, PredErrorModel) else pred_error_model)
 
 def generate_non_iid_clients(model_f_extractor: ModelFExtractor = None, model_sensors: list[ModelSensors] = [], verbose: bool = False) -> list[Client]:
 

federated_server.py
@@ -68,7 +68,7 @@ class Server:
         global_model = deepcopy(self.model_f_extractor)
         global_model.to(DEVICE)
 
-        global_state = global_model.state_dict()
+        global_state = deepcopy(global_model.state_dict())
 
         # Prepare accumulators
         for key in global_state.keys():
@@ -80,14 +80,14 @@ class Server:
         # Aggregate parameters
         for client in clients:
 
-            client_state = client.model_f_extractor.state_dict()
+            client_state = deepcopy(client.model_f_extractor.state_dict())
             weight = client.num_of_samples if weighted else 1
 
             for key in global_state.keys():
                 global_state[key] += (client_state[key] * (weight / total_weight))
 
         # Load averaged weights into global model
-        global_model.load_state_dict(global_state)
+        global_model.load_state_dict(deepcopy(global_state))
 
         return global_model
     
@@ -160,8 +160,8 @@ class Server:
             #! CHECKPOINT #######
             for client in self.clients:
                 torch.save({
-                    "model_f_extractor": client.model_f_extractor.state_dict(),
-                    "model_sensor": client.model_sensor.state_dict()
+                    "model_f_extractor": deepcopy(client.model_f_extractor.state_dict()),
+                    "model_sensor": deepcopy(client.model_sensor.state_dict())
                 }, join(round_path, f"{str(client.id)}.pt"))
             #!###################
 
@@ -213,9 +213,9 @@ class Server:
             
             #! CHECKPOINT #######
             torch.save({
-                "model_f_extractor": best_model_f_extractor.state_dict(),
+                "model_f_extractor": deepcopy(best_model_f_extractor.state_dict()),
                 "model_sensors": {
-                    str(client): client.model_sensor.state_dict()
+                    str(client): deepcopy(client.model_sensor.state_dict())
                 for client in self.clients}
             }, join(round_path, f"{WIDE_DEEP_NETWORK_BASENAME}.pt"))
             #!###################
@@ -245,9 +245,9 @@ class Server:
             client.set_model_f_extractor(model_f_extractor=self.model_f_extractor)
 
         torch.save({
-            "model_f_extractor": best_model_f_extractor.state_dict(),
+            "model_f_extractor": deepcopy(best_model_f_extractor.state_dict()),
             "model_sensors": {
-                str(client): client.model_sensor.state_dict()
+                str(client): deepcopy(client.model_sensor.state_dict())
             for client in self.clients}
         }, join(session_path, f"{WIDE_DEEP_NETWORK_BASENAME}.pt"))
 

models.py
@@ -23,9 +23,7 @@ class ModelFExtractor(nn.Module):
         self.relu = nn.LeakyReLU()
         self.dropout = nn.Dropout(p=0.4)
 
-        # Deep branch
-        self.fc13 = nn.Linear(window_size_in, 
-                              window_size_in * 3 if window_size_in >= n_devices_in else n_devices_in * 3)
+        self.fc13 = nn.Linear(window_size_in, window_size_in * 3 if window_size_in >= n_devices_in else n_devices_in * 3)
         self.conv = nn.Sequential(
             nn.Conv1d(n_devices_in, 64, kernel_size),
             nn.LeakyReLU(True),
@@ -35,17 +33,15 @@ class ModelFExtractor(nn.Module):
             nn.MaxPool1d(2)
         )
         self.conv_out_channels = 128
-        self.maxpool1_out = maxpool1d_output_shape(conv1d_output_shape(
-            window_size_in * 3 if window_size_in >= n_devices_in else n_devices_in * 3, kernel_size=kernel_size), kernel_size=2)
-        self.maxpool2_out = maxpool1d_output_shape(conv1d_output_shape(
-            self.maxpool1_out, kernel_size=kernel_size), kernel_size=2)
+        self.maxpool1_out = maxpool1d_output_shape(conv1d_output_shape(window_size_in * 3 if window_size_in >= n_devices_in else n_devices_in * 3, kernel_size=kernel_size), kernel_size=2)
+        self.maxpool2_out = maxpool1d_output_shape(conv1d_output_shape(self.maxpool1_out, kernel_size=kernel_size), kernel_size=2)
         self.out_2 = nn.Linear(self.conv_out_channels, window_size_out)
 
-        # Wide branch
         self.fc20 = nn.Linear(window_size_in, window_size_out)
 
-        # Aggregation
         self.out_h = nn.Linear(self.maxpool2_out + n_devices_in, 80)
+
+        self.n_devices_in = n_devices_in
 
     def forward_two(self, x):
         x = self.fc13(x)
@@ -63,8 +59,18 @@ class ModelFExtractor(nn.Module):
 
         if mask is not None:
             x_t_1 = x_t_1 * mask
+
             active = mask.sum(dim=2, keepdim=True)
-            x_t_1 = x_t_1 / active.clamp(min=1.0)
+
+            full = self.n_devices_in
+
+            normalize = (active < full)
+
+            x_t_1 = torch.where(
+                normalize,
+                x_t_1 / active.clamp(min=1.0),
+                x_t_1
+            )
 
         x_t_1 = x_t_1.transpose(2, 1)
         y_t2 = self.forward_two(x_t_1)

processing.py
@@ -1,4 +1,5 @@
 from constants import *
+import config
 
 from os import makedirs
 
@@ -37,26 +38,32 @@ def clean_dataset(src: str) -> pd.DataFrame:
 
 def normalize_datasets(*datasets: tuple[pd.DataFrame]) -> tuple[pd.DataFrame]:
 
-    # Vertically stack all the data
-    full_data = np.vstack([dataset[GLOBAL_INPUTS].values for dataset in datasets])
+    # Stack data across all datasets to compute global min/max
+    full_data = np.vstack([dataset[GLOBAL_INPUTS].to_numpy() for dataset in datasets])
 
-    # Get min and max value
-    min_v = np.minimum(full_data.min(axis=0), 0)
-    max_v = np.maximum(full_data.max(axis=0), 1)
+    # Compute min/max
+    min_v = full_data.min(axis=0)
+    max_v = full_data.max(axis=0)
 
-    # Normalize each dataset
-    normalize = lambda x: np.clip((x - min_v) / (max_v - min_v), 0, 1)
+    # Match SWaT handling:
+    # if min == max, force min = 0
+    # if max == 0, force max = 1
+    min_v = np.where(min_v == max_v, np.zeros_like(min_v), min_v)
+    max_v = np.where(max_v == 0., np.ones_like(max_v), max_v)
 
-    return (
-        pd.DataFrame(
-            data=np.hstack([
-                normalize(dataset[GLOBAL_INPUTS].values),
-                dataset[["Normal/Attack"]].values
-            ]),
+    # Lambda normalization (same as SWaT script)
+    normalize = lambda arr: np.clip((arr - min_v) / (max_v - min_v), 0, 1)
+
+    results = []
+    for dataset in datasets:
+        scaled = normalize(dataset[GLOBAL_INPUTS].to_numpy())
+        out = pd.DataFrame(
+            data=np.hstack([scaled, dataset[["Normal/Attack"]].to_numpy()]),
             columns=GLOBAL_INPUTS + ["Normal/Attack"]
         )
-        for dataset in datasets
-    )
+        results.append(out)
+
+    return tuple(results)
 
 def split_clients(df: pd.DataFrame) -> list[pd.DataFrame]:
 
@@ -181,7 +188,7 @@ if __name__ == "__main__":
 
         group = group_normal.create_group(f"client-{index}")
         group.attrs["columns"] = list(client.columns)
-        group.attrs["inputs"] = list(set(client.columns) - set(["Normal/Attack"]))
+        group.attrs["inputs"] = [column for column in list(client.columns) if column != "Normal/Attack"]
         group.attrs["outputs"] = [column for column in list(client.columns) if column in GLOBAL_OUTPUTS]
 
         group.create_dataset("df_normal_train", data=df_normal_train.values)
@@ -206,7 +213,7 @@ if __name__ == "__main__":
 
         group = group_attack.create_group(f"client-{index}")
         group.attrs["columns"] = list(client.columns)
-        group.attrs["inputs"] = list(set(client.columns) - set(["Normal/Attack"]))
+        group.attrs["inputs"] = [column for column in list(client.columns) if column != "Normal/Attack"]
         group.attrs["outputs"] = [column for column in list(client.columns) if column in GLOBAL_OUTPUTS]
 
         group.create_dataset("df_attack", data=client.values)
@@ -217,52 +224,52 @@ if __name__ == "__main__":
     hf.close()
 
     # DAICS
-    hf = h5py.File(name=OUTPUT_FILE_DAICS, mode="w")
-    group_normal = hf.create_group(f"normal")
-    group_attack = hf.create_group(f"attack")
+    # hf = h5py.File(name=OUTPUT_FILE_DAICS, mode="w")
+    # group_normal = hf.create_group(f"normal")
+    # group_attack = hf.create_group(f"attack")
 
-    df_normal_train, df_normal_val, df_normal_test = split_train_val_test(df=df_normal)
+    # df_normal_train, df_normal_val, df_normal_test = split_train_val_test(df=df_normal)
 
-    (
-        df_normal_train_input_indices, 
-        df_normal_train_output_indices, 
+    # (
+    #     df_normal_train_input_indices, 
+    #     df_normal_train_output_indices, 
         
-        df_normal_val_input_indices, 
-        df_normal_val_output_indices, 
+    #     df_normal_val_input_indices, 
+    #     df_normal_val_output_indices, 
         
-        df_normal_test_input_indices, 
-        df_normal_test_output_indices
-    ) = prepare_sliding_windows(df_train=df_normal_train, df_val=df_normal_val, df_test=df_normal_test)
+    #     df_normal_test_input_indices, 
+    #     df_normal_test_output_indices
+    # ) = prepare_sliding_windows(df_train=df_normal_train, df_val=df_normal_val, df_test=df_normal_test)
 
-    group_normal.attrs["columns"] = list(df_normal.columns)
-    group_normal.attrs["inputs"] = list(set(df_normal.columns) - set(["Normal/Attack"]))
-    group_normal.attrs["outputs"] = [column for column in list(df_normal.columns) if column in GLOBAL_OUTPUTS]
+    # group_normal.attrs["columns"] = list(df_normal.columns)
+    # group_normal.attrs["inputs"] = list(set(df_normal.columns) - set(["Normal/Attack"]))
+    # group_normal.attrs["outputs"] = [column for column in list(df_normal.columns) if column in GLOBAL_OUTPUTS]
 
-    group_normal.create_dataset("df_normal_train", data=df_normal_train.values)
-    group_normal.create_dataset("df_normal_val", data=df_normal_val.values)
-    group_normal.create_dataset("df_normal_test", data=df_normal_test.values)
+    # group_normal.create_dataset("df_normal_train", data=df_normal_train.values)
+    # group_normal.create_dataset("df_normal_val", data=df_normal_val.values)
+    # group_normal.create_dataset("df_normal_test", data=df_normal_test.values)
 
-    group_normal.create_dataset("df_normal_train_input_indices", data=df_normal_train_input_indices)
-    group_normal.create_dataset("df_normal_train_output_indices", data=df_normal_train_output_indices)
+    # group_normal.create_dataset("df_normal_train_input_indices", data=df_normal_train_input_indices)
+    # group_normal.create_dataset("df_normal_train_output_indices", data=df_normal_train_output_indices)
 
-    group_normal.create_dataset("df_normal_val_input_indices", data=df_normal_val_input_indices)
-    group_normal.create_dataset("df_normal_val_output_indices", data=df_normal_val_output_indices)
+    # group_normal.create_dataset("df_normal_val_input_indices", data=df_normal_val_input_indices)
+    # group_normal.create_dataset("df_normal_val_output_indices", data=df_normal_val_output_indices)
 
-    group_normal.create_dataset("df_normal_test_input_indices", data=df_normal_test_input_indices)
-    group_normal.create_dataset("df_normal_test_output_indices", data=df_normal_test_output_indices)
+    # group_normal.create_dataset("df_normal_test_input_indices", data=df_normal_test_input_indices)
+    # group_normal.create_dataset("df_normal_test_output_indices", data=df_normal_test_output_indices)
 
-    (
-        df_attack_input_indices,
-        df_attack_output_indices
-    ) = prepare_sliding_windows(df_test=df_attack)
+    # (
+    #     df_attack_input_indices,
+    #     df_attack_output_indices
+    # ) = prepare_sliding_windows(df_test=df_attack)
 
-    group_attack.attrs["columns"] = list(df_attack.columns)
-    group_attack.attrs["inputs"] = list(set(df_attack.columns) - set(["Normal/Attack"]))
-    group_attack.attrs["outputs"] = [column for column in list(df_attack.columns) if column in GLOBAL_OUTPUTS]
+    # group_attack.attrs["columns"] = list(df_attack.columns)
+    # group_attack.attrs["inputs"] = list(set(df_attack.columns) - set(["Normal/Attack"]))
+    # group_attack.attrs["outputs"] = [column for column in list(df_attack.columns) if column in GLOBAL_OUTPUTS]
 
-    group_attack.create_dataset("df_attack", data=df_attack.values)
+    # group_attack.create_dataset("df_attack", data=df_attack.values)
 
-    group_attack.create_dataset("df_attack_input_indices", data=df_attack_input_indices)
-    group_attack.create_dataset("df_attack_output_indices", data=df_attack_output_indices)
+    # group_attack.create_dataset("df_attack_input_indices", data=df_attack_input_indices)
+    # group_attack.create_dataset("df_attack_output_indices", data=df_attack_output_indices)
 
-    hf.close()
+    # hf.close()